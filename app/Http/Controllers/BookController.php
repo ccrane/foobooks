d4c686d0f06cf3c4e1e609572c97deff120692ca
Chris Crane <chris.crane@bellaliant.net>
2018-11-28 20:29:31 -0330

    adding db access

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -19,6 +19,7 @@ class BookController extends Controller
      */
     public function show($title)
     {
+        dump(config('mail.supportEmail'));
         return view('books.show')->with(['title' => $title]);
     }
 }
